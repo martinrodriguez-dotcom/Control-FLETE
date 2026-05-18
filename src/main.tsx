1ac897fa10311b600854b3b87ce2a7ef948ce261
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2026-05-18 13:00:27 -0300

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Aquí deben ir las directivas de Tailwind
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
