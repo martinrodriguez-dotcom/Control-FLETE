4aec2db830c796c19de4857594d7126a99d15a91
martinrodriguez-dotcom <martin.rodriguez@siipallets.com>
2026-05-18 12:58:04 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', // Habilita el modo oscuro controlándolo mediante la clase 'dark' en el elemento html
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
